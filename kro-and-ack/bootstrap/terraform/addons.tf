eea00a4a9915ab4d2bbaea6ea95c5477f52e0fbe
7navyasa <140163168+7navyasa@users.noreply.github.com>
2025-04-03 11:21:48 +1100

    Update addons.tf revert alb controller

--- kro-and-ack/bootstrap/terraform/addons.tf
+++ kro-and-ack/bootstrap/terraform/addons.tf
@@ -19,8 +19,6 @@ module "eks_blueprints_addons" {
       templatefile("${path.module}/helm-values/argocd.yaml", {
     })]
   }
-
-  enable_aws_load_balancer_controller = true
   enable_metrics_server               = true
   enable_external_secrets             = true
   enable_external_dns                 = true
